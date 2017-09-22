593e9d1d9b9ab6039fa5a5ec02c02fe9398466e1
qpq-u32 <qiuworker@163.com>
2017-09-22 15:15:47 +0800

    init

---
+++ build.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+
+echo make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi- zynq_zturn_config
+make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi- zynq_zturn_config
+
+echo make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+
---
+++ cleanmake.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi- distclean
